c262c19dc72e16d3fecd6e6d1281f25158ccec6d
Noxmor <janek.drews@t-online.de>
2022-06-19 18:48:39 +0200

    Added Assert and Vector iterators

--- NoxData/src/NoxData/Array.hpp
+++ NoxData/src/NoxData/Array.hpp
@@ -4,9 +4,124 @@
 
 namespace NoxData {
 
+	template<typename Array>
+	class ArrayIterator
+	{
+	public:
+		using ValueType = typename Array::ValueType;
+		using PointerType = ValueType*;
+		using ReferenceType = ValueType&;
+
+	public:
+		ArrayIterator(const PointerType ptr) noexcept
+			: m_Ptr(ptr)
+		{
+		}
+
+		~ArrayIterator() noexcept = default;
+
+		ArrayIterator& operator++() noexcept
+		{
+			m_Ptr++;
+			return *this;
+		}
+		
+		ArrayIterator operator++(int) noexcept
+		{
+			ArrayIterator it = *this;
+			++(*this);
+			return it;
+		}
+		
+		ArrayIterator& operator--() noexcept
+		{
+			m_Ptr--;
+			return *this;
+		}
+		
+		ArrayIterator operator--(int) noexcept
+		{
+			ArrayIterator it = *this;
+			--(*this);
+			return it;
+		}
+		
+		ND_NODISCARD ReferenceType operator[](const int index) noexcept { return *(m_Ptr[index]); }
+
+		ND_NODISCARD PointerType operator->() const noexcept { return m_Ptr; }
+		
+		ND_NODISCARD ReferenceType operator*() noexcept { return *m_Ptr; }
+		
+		ND_NODISCARD bool operator==(const ArrayIterator& other) const noexcept { return m_Ptr == other.m_Ptr; }
+		ND_NODISCARD bool operator!=(const ArrayIterator& other) const noexcept { return !(*this == other); }
+		
+	private:
+		PointerType m_Ptr;
+	};
+
+	template<typename Array>
+	class ArrayConstIterator
+	{
+	public:
+		using ValueType = typename Array::ValueType;
+		using PointerType = const ValueType*;
+		using ReferenceType = const ValueType&;
+
+	public:
+		ArrayConstIterator(const PointerType ptr) noexcept
+			: m_Ptr(ptr)
+		{
+		}
+
+		~ArrayConstIterator() noexcept = default;
+
+		ArrayConstIterator& operator++() noexcept
+		{
+			m_Ptr++;
+			return *this;
+		}
+
+		ArrayConstIterator operator++(int) noexcept
+		{
+			ArrayConstIterator it = *this;
+			++(*this);
+			return it;
+		}
+
+		ArrayConstIterator& operator--() noexcept
+		{
+			m_Ptr--;
+			return *this;
+		}
+
+		ArrayConstIterator operator--(int) noexcept
+		{
+			ArrayConstIterator it = *this;
+			--(*this);
+			return it;
+		}
+
+		ND_NODISCARD ReferenceType operator[](const int index) noexcept { return *(m_Ptr[index]); }
+
+		ND_NODISCARD PointerType operator->() const noexcept { return m_Ptr; }
+
+		ND_NODISCARD ReferenceType operator*() noexcept { return *m_Ptr; }
+
+		ND_NODISCARD bool operator==(const ArrayConstIterator& other) const noexcept { return m_Ptr == other.m_Ptr; }
+		ND_NODISCARD bool operator!=(const ArrayConstIterator& other) const noexcept { return !(*this == other); }
+
+	private:
+		PointerType m_Ptr;
+	};
+
 	template<typename T, size_t N>
 	class Array
 	{
+	public:
+		using ValueType = T;
+		using Iterator = ArrayIterator<Array<T, N>>;
+		using ConstIterator = ArrayConstIterator<Array<T, N>>;
+
 	public:
 		Array() noexcept = default;
 		~Array() noexcept = default;
@@ -18,21 +133,21 @@ namespace NoxData {
 
 		ND_NODISCARD T& operator[](const size_t index) noexcept
 		{
-#ifdef ND_DEBUG
-			if (index >= N)
-				ND_DEBUGBREAK();
-#endif
+			ND_ASSERT(index < N);
 			return m_Data[index];
 		}
 
 		ND_NODISCARD const T& operator[](const size_t index) const noexcept
 		{
-#ifdef ND_DEBUG
-			if (index >= N)
-				ND_DEBUGBREAK(); 
-#endif
+			ND_ASSERT(index < N);
 			return m_Data[index];
 		}
+
+		Iterator begin() noexcept { return Iterator(&m_Data[0]); }
+		ConstIterator cbegin() noexcept { return ConstIterator(&m_Data[0]); }
+
+		Iterator end() noexcept { return Iterator(&m_Data[N]); }
+		ConstIterator cend() noexcept { return ConstIterator(&m_Data[N]); }
 
 	private:
 		T m_Data[N];
--- NoxData/src/NoxData/Core/Core.hpp
+++ NoxData/src/NoxData/Core/Core.hpp
@@ -3,6 +3,16 @@
 namespace NoxData {
 
 #define ND_NODISCARD [[nodiscard]]
+
 #define ND_DEBUGBREAK() __debugbreak()
 
+#ifdef ND_DEBUG
+#define ND_ENABLE_ASSERTS
+#endif
+
+#ifdef ND_ENABLE_ASSERTS
+#define ND_ASSERT(x, ...) { if(!(x)) { ND_DEBUGBREAK(); } }
+#else
+#define ND_ASSERT(x, ...)
+#endif
 }
